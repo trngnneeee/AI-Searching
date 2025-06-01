0bb7a581ed2c6084f7d669a959ce9e2cb31254a3
Nguyen Trong Tai <taitrong987@gmail.com>
2025-06-01 11:57:47 +0700

    Draw

---
+++ main.py
@@ -0,0 +1,87 @@
+import turtle 
+import math 
+import time
+
+window = turtle.Screen()
+window.bgcolor("black")
+window.title ("My team's maze game!")
+window.setup(700, 700)
+
+#Register shapes
+turtle.register_shape("goku_right.gif")
+turtle.register_shape("goku_left.gif")
+turtle.register_shape("treasure.gif")
+turtle.register_shape("wall.gif")
+
+#Create Pen
+class Pen(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.speed(0)
+
+#Create levels list
+levels = [""]
+
+#Define first level
+level_1 = [
+"XXXXXXXXXXXXXXXXXXXXXXXX",
+"    X                  X",
+"XXX X XXXXXXXXXXXXXXXX X",
+"X   X X             X X",
+"X X X XXXXXXXXXXXXX X X",
+"X X   X           X X X",
+"X X XXX XXXXXXXX X X X",
+"X X X   X       X X X X",
+"X X XXXXXXXXXXXX X X X",
+"X X   X       X X X X X",
+"X XXXXX XXXXXX X X X X",
+"X       X     X   X X X",
+"XXXXXXXXX XX XXXXXXX X X",
+"X         X         X X",
+"X XXXXXXXXX XXXXXXXX X X",
+"X X               X     X",
+"X X XXXXXXXXXXXXXXXX XXXX",
+"X X X             X   XX",
+"X X XXXXXXXXXXXXXXXX XXX",
+"X   X               X X",
+"XXXXXXXXXXXXXXX XXXXX X",
+"X                 X X X",
+"X XXXXXXXXXXXXXXX X X X",
+"X                 X   X",
+"XXXXXXXXXXXXXXXXXXXXXXXX"
+]
+
+#Add maze to mazes list 
+levels.append(level_1)
+
+#Create Level Setup Function
+def setup_maze(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+
+            character = level[y][x]
+
+            screen_x = -288 + (x * 24)
+            screen_y = 288 - (y * 24)
+
+            if character == "X":
+                pen.goto(screen_x, screen_y)
+                pen.stamp()
+
+            #if character == "P":
+               # pen.goto(screen_x, screen_y)
+
+#Create class instance
+pen = Pen()
+
+#Set up the level
+setup_maze(levels[1])
+
+#Main game loop
+while True:
+    pass
+
+
