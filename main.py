3f2a17c8a30331fabab74a699a2658e06ef71dd6
khoavadienq <ddkhoa9@gmail.com>
2025-06-13 00:25:31 +0700

    add gen maze button

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle 
-import tkinter
+import tkinter as tk
 import math 
 import time
 import maze
@@ -11,13 +11,14 @@ window = turtle.Screen()
 window.bgcolor("black")
 window.title ("My team's maze game!")
 window._root.attributes("-fullscreen", True)
-
 def exit_fullscreen():
     window._root.attributes("-fullscreen", False)
 
 window.listen()
 window.onkey(exit_fullscreen, "Escape")
-        
+
+root = window._root
+
 #Gen Maze
 screen_width = window._root.winfo_screenwidth()
 screen_height = window._root.winfo_screenheight()
@@ -25,6 +26,25 @@ screen_height = window._root.winfo_screenheight()
 maze_width = (screen_width // 32) | 1
 maze_height = (screen_height // 32) | 1
 level = maze.generateMaze(maze_height, maze_width)
+
+def regenerateMaze():
+    global level
+    level = maze.generateMaze(maze_height, maze_width)
+    turtle.clearscreen() 
+    turtle.bgcolor("black")
+    window.title("My team's maze game!")
+
+    maze.setup_maze(level)
+    DFS.drawMazeDFS(level)
+
+#button
+#gen map button
+gen_maze_button = tk.Button(root, text = "Generate maze", font=("Arial", 24, "bold"),
+                        bg="red", fg="white", command=lambda : regenerateMaze())
+
+gen_maze_button.place(relx=1.0, rely=0.0, anchor="ne", x=-40, y=40)
+
+
 
 #Set up the level
 maze.setup_maze(level)
